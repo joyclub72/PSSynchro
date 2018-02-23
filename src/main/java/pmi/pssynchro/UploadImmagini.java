6de3e7fc2ff96d6e58fd08668201777b448672b5
nicola <joyclub72@gmail.com>
2018-02-23 21:59:00 +0100

    Continua gestione upload immagini via ftp

--- src/main/java/pmi/pssynchro/UploadImmagini.java
+++ src/main/java/pmi/pssynchro/UploadImmagini.java
@@ -12,6 +12,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
 import org.apache.commons.net.PrintCommandListener;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPConnectionClosedException;
@@ -20,7 +21,7 @@ import org.apache.commons.net.ftp.FTPSClient;
 
 public class UploadImmagini {
 
-    public void FtpUpload(String server, String username, String password, String percorsoRemoto, String percorsoLocale, String fileImmagine, String all, String check) throws NoSuchAlgorithmException {
+    public void FtpUpload(String server, String username, String password, String percorsoRemoto, String percorsoLocale, String fileImmagine, String all, String check) throws NoSuchAlgorithmException, InterruptedException {
         boolean storeFile = false, binaryTransfer = false, error = false;
         String remote, local;
         String protocol = "TLS";    // SSL/TLS
@@ -34,34 +35,38 @@ public class UploadImmagini {
 
         ftps = new FTPSClient(protocol);
         ftps.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
-            try {
-                sito = new URL(sUrl + "?aggiornamento=arimmagini1" + all);
-            } catch (MalformedURLException ex) {
-                System.out.println("Indirizzo del sito mal formato o inesistente");
-            }
-            URLConnection yc = null;
-            try {
-                yc = sito.openConnection();
-                System.out.println(sito + " - Apertura connessione");/*test*/
-            } catch (IOException ex) {
-                System.out.println("Errore di connessione _ ");
-            }
-            BufferedReader in;
-            try {
-                in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-                System.out.println(sito + " - Comunicazione");/*test*/
-            } catch (IOException ex) {
-                System.out.println(": Errore in ricezione dati: verificare che il "
-                        + "server sia avviato o che l'indirizzo sia corretto");
-                return;
-            }
-        /*Stame di test*/
+        try {
+            sito = new URL(sUrl + "?aggiornamento=arimmagini1" + all);
+        } catch (MalformedURLException ex) {
+            System.out.println("Indirizzo del sito mal formato o inesistente");
+        }
+        URLConnection yc = null;
+        try {
+            yc = sito.openConnection();
+            System.out.println(sito + " - Apertura connessione");/*test*/
+        } catch (IOException ex) {
+            System.out.println("Errore di connessione _ ");
+        }
+        BufferedReader in;
+        try {
+            in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+            System.out.println(sito + " - Comunicazione");/*test*/
+        } catch (IOException ex) {
+            System.out.println(": Errore in ricezione dati: verificare che il "
+                    + "server sia avviato o che l'indirizzo sia corretto");
+            return;
+        }
+        /*Stampe di test*/
         System.out.println(percorsoLocale);
         System.out.println(check);
         System.out.println(Varie.esiste(check));
         System.out.println(all);
         /*Fine stampe di test*/
-        
+
+        while (!(Varie.esiste(check))) {
+            TimeUnit.SECONDS.sleep(5);
+        }
+        /*Procedura di upload vera e propria*/
         try {
             int reply;
 
@@ -139,6 +144,7 @@ public class UploadImmagini {
             }
         }
         //System.exit(error ? 1 : 0);
+        /*Fine procedura upload vera e propria*/
     } // end main    
 
 }
